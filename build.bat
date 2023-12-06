0c36878ae021f39128682e3736d21a11e3d94022
nnkgw <renatus0407@outlook.com>
2023-12-06 13:43:22 +0900

    add build command

--- build.bat
+++ build.bat
@@ -1,1 +1,7 @@
 zcc +msx -create-app -O3 -DAMALLOC -subtype=msxdos main.c libiot.c -bn ENV.COM -lm
+del ENV.img
+move /y ENV.COM bin
+cd bin
+copy dosformsx.dsk ENV.DSK
+START /WAIT DISKMGR.exe -A -F -C ENV.DSK ENV.COM
+cd ../
--- debug_build.bat
+++ debug_build.bat
@@ -1,7 +1,1 @@
 zcc +msx -create-app -O0 -m -debug -DAMALLOC -subtype=msxdos -lm -v -bn ENV.COM main.c libiot.c 
-rename ENV.img ENV.DSK
-move /y ENV.DSK bin
-cd bin
-START /WAIT DISKMGR.exe -A -F -C ENV.DSK COMMAND.COM
-START /WAIT DISKMGR.exe -A -F -C ENV.DSK DOS.SYS
-cd ../
